fd976fb66f82b631d619a50313b0c74bdd39bb8d
EricCartman598 <morozov_spmt@mail.ru>
2020-11-08 13:52:11 +0300

    1. Added jsp controller

--- src/main/java/com/home/ExpensesControl/user/controller/UserController.java
+++ src/main/java/com/home/ExpensesControl/user/controller/UserController.java
@@ -1,17 +1,14 @@
 package com.home.ExpensesControl.user.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
-
     @GetMapping("/{id}")
-    public String getUserById(@PathVariable("id") long id) {
-        System.out.println("user controller invoked!");
-        return "user controller invoked!";
+    public String welcome() {
+        return "user";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/main.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Main page</title>
+</head>
+<body>
+    <p>This is the main page777</p>
+</body>
+</html>
